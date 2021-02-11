ab28b5871177b4a04923f73518102a83dbc9f482
icrslb <icrsbarbosa@gmail.com>
2021-02-10 21:52:29 -0300

    Inserção de threads

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 import pygame
 #from pygame.local import *
 
+import threading
+from threading import Thread
+
 import numpy as np
 import funcoes_proc_img as pi
 import negativo as neg
 import gamma
 import linear_partes
+
+class Dados :
+    def __init__ ( self ):
+        self.I = None
+        self.I_aux = None
+
+        self.mostrar_I = True
+        self.tamanho = None
+
+        self.mutex = threading.Semaphore( 1 )
+
+class Tela ( Thread ):
+    def __init__ ( self, _dados, _tela ):
+        Thread.__init__( self )
+        self.dados = _dados
+        self.tela = _tela
+
+        self.rodar = True
+    
+    def run ( self ):
+        while ( self.rodar ):
+
+            self.dados.mutex.acquire()
+            
+            if not ( self.dados.I is None ):
+                atualiza_tela( self.dados.I, self.tela, self.dados.tamanho )
+
+            self.dados.mutex.release()
+
+            pygame.display.flip()
+    
+    def terminar ( self ):
+        self.rodar = False
 
 def atualiza_tela ( I, tela, tamanho ):
     surface = pygame.surfarray.make_surface( 255 * I.transpose( 1, 0, 2 ) )
@@ -15,8 +51,9 @@ def atualiza_tela ( I, tela, tamanho ):
     tela.blit( surface, [ 0, 0 ] )
 
 def main ():
-    tamanho = [ 0, 0 ]
-    I = None
+    dados = Dados()
+    tr = Tela( dados, tela )
+    tr.start()
 
     while ( True ):
         opcao = -1
@@ -26,39 +63,41 @@ def main ():
 
             print ( "-1: Sair\n"
                   + " 0: Abrir Imagem\n"
-                  + " 1: Mostrar imagem\n"
-                  + " 2: Negativo\n"
-                  + " 3: Transformações logarítimicas\n"
-                  + " 4: Correção de gama\n"
-                  + " 5: Linear por partes\n" )
+                  + " 1: Negativo\n"
+                  + " 2: Transformações logarítimicas\n"
+                  + " 3: Correção de gama\n"
+                  + " 4: Linear por partes\n" )
 
             opcao = int( input ( ": " ) )
 
             if ( opcao == -1 ):
+                tr.terminar()
                 return
 
         if ( opcao == 0 ):
             caminho = input ( "Insira o caminho: " )
-            I = pi.ler_imagem( caminho )
+            
+            dados.mutex.acquire()
+            
+            dados.I = pi.ler_imagem( caminho )
+            dados.tamanho = largura, altura = [ larg_t, int(larg_t * ( dados.I.shape[0] / dados.I.shape[1] ) ) ]
 
-            tamanho = largura, altura = [ larg_t, int(larg_t * ( I.shape[0] / I.shape[1] ) ) ]
+            dados.mutex.release()
         elif ( opcao == 1 ):
-            pi.mostrar_imagem( I )
+            dados.I = neg.negativo( dados.I )
         elif ( opcao == 2 ):
-            I = neg.negativo( I )
+            valor = float( input ( "Insira o valor: " ) )
+            dados.I = pi.ler_imagem( "Imagens/Pinguim_1.jpg" )
         elif ( opcao == 3 ):
             valor = float( input ( "Insira o valor: " ) )
-            I = pi.ler_imagem( "Imagens/Pinguim_1.jpg" )
-        elif ( opcao == 4 ):
-            valor = float( input ( "Insira o valor: " ) )
-            I = gamma.correcao_gamma( I, valor )
-        elif ( opcao == 5 ):
-            I = pi.ler_imagem( "Imagens/Pinguim_1.jpg" )
-        
-        if not ( I is None ):
-            atualiza_tela( I, tela, tamanho )
 
-        pygame.display.flip()
+            dados.mutex.acquire()
+
+            dados.I = gamma.correcao_gamma( dados.I, valor )
+
+            dados.mutex.release()
+        elif ( opcao == 4 ):
+            dados.I = pi.ler_imagem( "Imagens/Pinguim_1.jpg" )
 
         print ( "\n" )
 
