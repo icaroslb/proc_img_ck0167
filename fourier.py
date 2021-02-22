df754abf287d2be8f246e70b14f162b4f1388d0b
icrslb <icrsbarbosa@gmail.com>
2021-02-22 18:10:59 -0300

    Operações com fourier e transformações de escala

--- fourier.py
+++ fourier.py
@@ -36,3 +36,110 @@ def transformada_inversa_fourier ( imagem ):
                 matriz_transformada[i][j][k] = ( 1 / ( dim[0] * dim[1] ) ) * np.sum( imagem[ :, :, k ] * matriz_exp[ :, :, k ] )
     #matriz_transformada = np.real( np.fft.ifft2( imagem ) )
     return np.real( matriz_transformada )
+
+def transformada_passa_baixa_dura( ondas, raio ):
+    ondas_passadas = np.copy( ondas )
+
+    dim = ondas_passadas.shape
+    meio = int( dim[0] / 2 ), int( dim[1] / 2 )
+
+    for i in range( dim[0] ):
+        for j in range( dim[1] ):
+            for k in range( dim[2] ):
+                if( ( abs( i - meio[0] ) + abs( j - meio[1] ) ) > raio ):
+                    ondas_passadas[i][j][k] = 0
+
+    return ondas_passadas
+
+def transformada_passa_baixa_suave( ondas, raio, raio_suave ):
+    ondas_passadas = np.copy( ondas )
+
+    dim = ondas_passadas.shape
+    meio = int( dim[0] / 2 ), int( dim[1] / 2 )
+
+    for i in range( dim[0] ):
+        for j in range( dim[1] ):
+            for k in range( dim[2] ):
+                dist = abs( i - meio[0] ) + abs( j - meio[1] )
+                if( dist > raio ):
+                    ondas_passadas[i][j][k] = ondas[i][j][k] * max( ( raio_suave - ( dist - raio ) ) / raio_suave, 0 )
+    
+    return ondas_passadas
+
+def transformada_passa_alta_dura( ondas, raio ):
+    ondas_passadas = np.copy( ondas )
+
+    dim = ondas_passadas.shape
+    meio = int( dim[0] / 2 ), int( dim[1] / 2 )
+
+    for i in range( dim[0] ):
+        for j in range( dim[1] ):
+            for k in range( dim[2] ):
+                if( ( abs( i - meio[0] ) + abs( j - meio[1] ) ) < raio ):
+                    ondas_passadas[i][j][k] = 0
+    
+    return ondas_passadas
+
+def transformada_passa_alta_suave( ondas, raio, raio_suave ):
+    ondas_passadas = np.copy( ondas )
+
+    dim = ondas_passadas.shape
+    meio = int( dim[0] / 2 ), int( dim[1] / 2 )
+
+    for i in range( dim[0] ):
+        for j in range( dim[1] ):
+            for k in range( dim[2] ):
+                dist = abs( i - meio[0] ) + abs( j - meio[1] )
+                if( dist < raio ):
+                    ondas_passadas[i][j][k] = ondas[i][j][k] * max( ( raio_suave - ( raio - dist ) ) / raio_suave, 0 )
+    
+    return ondas_passadas
+
+def transformada_terminal( dados ):
+    while ( True ):
+        print( "\nEscolha o filtro:\n"
+             + "1 - Passa baixa dura\n"
+             + "2 - Passa alta dura\n"
+             + "3 - Passa faixa dura\n"
+             + "4 - Passa baixa suave\n"
+             + "5 - Passa alta suave\n"
+             + "6 - Passa faixa suave\n" )
+
+        opcao = int( input( ": " ) )
+
+        if ( opcao < 1 or opcao > 6 ):
+            print( "\nOpção inexistente!!\n" )
+        else:
+            break
+    
+    ondas = np.zeros( dados.I.shape, dtype = complex )
+    
+    for i in range( dados.I.shape[2] ):
+        ondas[ :, :, i ] = np.fft.fft2( dados.I[ :, :, i ] )
+        ondas[ :, :, i ] = np.fft.fftshift( ondas[ :, :, i ] )
+    
+    
+    if ( opcao == 1 or opcao == 3 ):
+        raio = float( input( "Insira o raio passa baixa: " ) )
+        ondas = transformada_passa_baixa_dura( ondas, raio )
+    
+    if ( opcao == 2 or opcao == 3 ):
+        raio = float( input( "Insira o raio passa alta: " ) )
+        ondas = transformada_passa_alta_dura( ondas, raio )
+    
+    if ( opcao == 4 or opcao == 6 ):
+        raio = float( input( "Insira o raio passa baixa: " ) )
+        raio_suave = float( input( "Insira o raio de suavização: " ) )
+        ondas = transformada_passa_baixa_suave( ondas, raio, raio_suave )
+    
+    if ( opcao == 5 or opcao == 6 ):
+        raio = float( input( "Insira o raio passa alta: " ) )
+        raio_suave = float( input( "Insira o raio de suavização: " ) )
+        ondas = transformada_passa_alta_suave( ondas, raio, raio_suave )
+
+    
+    for i in range( 3 ):
+        ondas[ :, :, i ] = np.fft.fftshift( ondas[ :, :, i ] )
+        ondas[ :, :, i ] = np.real( np.fft.ifft2( ondas[ :, :, i ] ) )
+
+    dados.I = np.real( ondas )
