f867e756069e8f1599309905375554a747ba552c
Patautista <caleb.kart@gmail.com>
2021-03-18 22:42:38 -0300

    lzw

--- Compressor/lzw.cpp
+++ Compressor/lzw.cpp
@@ -1,66 +1,61 @@
 #include <iostream>
 #include <array>
+#include <string>
 #include <vector>
 using namespace std;
 
-template <class T>
-void print(std::vector <T> const &a) {
-   std::cout << "The vector elements are : ";
-
-   for(int i=0; i < a.size(); i++)
-   std::cout << a.at(i) << ' ';
-}
-
-template <class T>
-T* merge(T* a, T* b){
-    T* c = new T[sizeof(a)+sizeof(b)];
-    for(int i = 0; i<= sizeof(a)-1; i++){
-        c[i] = a[i];
-    }
-    for(int i = 0; i<= sizeof(b)-1; i++){
-        c[sizeof(a)+i] = b[i];
-    }
-    return c;
-}
 
 int main(){
-    vector<unsigned char*> dict;
-    dict.resize(256);
+    vector<string> dict {};
+    //Popula o 
     for(int val = 0; val <= 255; val++){
-        unsigned char* ascii = new unsigned char[1];
-        ascii[0] = (unsigned char)('0' + val);
-        dict[val] = ascii;
-        //cout << *dict[val];
+        string ascii;
+        ascii = ('0' + val);
+        dict.push_back(ascii);
+        cout << dict.at(val);
     }
-    unsigned char init[] = "";
-    unsigned char* I = init;
 
-    unsigned char* c = new unsigned char[1];
+    string I = "";
 
-    vector<unsigned char> seqcod;
-    unsigned char teste[] = "A_ASA_DA_CASA";
+    char c;
 
-    for(int i = 0; i<=sizeof(teste)-1; i++){
-        *c = teste[i];
-        for(int j = 0; j<=dict.size()-1; j++){
-            unsigned char* sum = merge<unsigned char>(I, c);
-            //cout << *sum << "vs" << *dict[j] << "\n";
-            if(*sum == *dict[j]){
-                //cout << "opa deu igual";
-                *I = *sum;
+    string seqcod;
+    string teste= "A_ASA_DA_CASA";
+
+    dict.resize(256);
+    for(int i = 0; i <= teste.length()-1; i++){
+    
+        c = teste.at(i);
+        bool exist = false;
+        cout << "round " << i << " I: " << I << "\n";
+        string sum = I + c;
+        //Procura a string no dicionário
+        int j;
+        for(j = 0; j<=dict.size()-1 && !exist; j++){
+            if(sum.compare(dict.at(j)) == 0){
+                cout << "opa, este existe \n";
+                cout << sum << "vs" << dict.at(j) << "\n";
+                I = sum;
+                exist = true;
             }
             else{
-                //i. coloque a palavra código correspondente a I na sequência codificada;
-                seqcod.insert( seqcod.end(), *I);
-		        //ii. adicione a string I+c ao dicionário;
-                dict.insert(dict.end(), sum);
-		        //iii. I <= c;
-                *I = *c;
-                
+                //cout << sum << "vs" << dict.at(j) << "\n";
             }
+        }   
+        if(!exist){
+            //i. coloque a palavra código correspondente a I na sequência codificada;
+            seqcod = seqcod + I;
+            //cout << "code = " <<(char)('0' + j) << "\n";
+            cout << "seqcod = " << seqcod << "\n";
+		    //ii. adicione a string I+c ao dicionário;
+            dict.push_back(sum);
+            cout << "Inserido: " << dict.at(dict.size()-1) << "\n";
+            //cout << "Inserido \n";
+		    //iii. I <= c;
+            I = c;
         }
+        cout << j;
     }
-    print(seqcod);
 
     return 0;
 }
