db1d525ccc2f20abcbf19c4241a4f09ba988c9bb
Patautista <caleb.kart@gmail.com>
2021-03-21 20:19:38 -0300

    Correções no descompressor

--- Compressor/lzw.cpp
+++ Compressor/lzw.cpp
@@ -2,113 +2,80 @@
 #include <array>
 #include <string>
 #include <vector>
+#include <map>
+#include <unordered_map>
 #include <windows.h>
 using namespace std;
 
-
-vector<int> lzwCompressor(string teste){
-    //Codificador
-    //cout << "Codificador \n";
-    vector<string> dict {};
-    //Popula o dionário
-    for(int val = 0; val <= 255; val++){
-        string ascii;
-        ascii = ('0' + val);
-        dict.push_back(ascii);
-        //cout << dict.at(val);
+string lzwDescompressor(vector<int> entrada) 
+{ 
+    string decom = "";
+    unordered_map<int, string> dict; 
+    for (int i = 0; i <= 255; i++) { 
+        string ascii = ""; 
+        ascii += char(i); 
+        dict[i] = ascii; 
+    } 
+    int pw = entrada[0], n; 
+    string s = dict[pw]; 
+    string c = "";
+    c += s[0]; 
+    decom += c;
+    int count = 256; 
+    for (int i = 0; i < entrada.size() - 1; i++) { 
+        n = entrada[i + 1]; 
+        if (dict.find(n) == dict.end()) { 
+            s = dict[pw]; 
+            s = s + c; 
+        } 
+        else { 
+            s = dict[n]; 
+        } 
+        //cout << s;
+        decom += s; 
+        c = ""; 
+        c += s[0]; 
+        dict[count] = dict[pw] + c; 
+        count++; 
+        pw = n; 
     }
-
-
-
-    vector<int> seqcod;
-    string I = "";
-    I = I + teste.at(0);
-    char c;
-
-    dict.resize(256);
-    seqcod.resize(0);
-    //cout << "dict.size() é: " << dict.size() << "\n";
-    int i = 1;
-    int track;
-    while (i <= teste.length())
-    {
-        if(i < teste.length()){
-            c = teste.at(i);
-        }
-        //cout << "c: " << c << "\n";
-        bool exist = false;
-        //cout << "round " << i << " I: " << I << "\n";
-        //cout << "I: " << I << "\n";
-        //Procura a string no dicionário
-        int j;
-        for(j = 0; j<=dict.size()-1 && exist == false; j++){
-            if(I.compare(dict.at(j)) == 0){
-                //cout << "opa, este existe \n";
-                //cout << I << "vs" << dict.at(j) << "\n";
-                track = j;
-                I = I + c;
-                i++;
-                exist = true;
-            }
-        }   
-        if(!exist){
-            //i. coloque a palavra código correspondente a I na sequência codificada;
-            seqcod.push_back(track);
-            //cout << "o tamanho de seqcod é " << seqcod.size() << "(" << dict.at(track) << " foi inserido)\n";
-		    //ii. adicione a string I+c ao dicionário;
-            dict.push_back(I);
-            //cout << I <<" foi inserido no dicionário: " << dict.at(dict.size()-1) << "\n";
-            //cout << "Inserido \n";
-		    //iii. I <= c;
-            I = teste.at(i-1);
-        }
-    }
-    seqcod.push_back(track);
-    //cout << "o tamanho de seqcod é " << seqcod.size() << "\n \n";
-    return seqcod;
+    return decom; 
 }
-string lzwDescompressor(vector<int>seqcod){
-    //cout << "Decodificador \n";
-    vector<string> dict {};
-    //Popula o dionário
-    for(int val = 0; val <= 255; val++){
-        string ascii;
-        ascii = ('0' + val);
-        dict.push_back(ascii);
-        //cout << dict1.at(val);
-    }
-    int cw = seqcod.at(0);
-    string str = "";
-    str = str + (char)('0' + cw);
-    string P;
-    string C;
-    
-    for(int i = 1; i <= seqcod.size()-1; i++){
-        int pw = cw;
-        cw = seqcod.at(i);
-        string cstr = "";
-        P = "";
-        cstr = cstr + (char)('0' + cw);
-        //Checa se cw é um índice no dicionário
-        if(cw <= dict.size()-1){
-            //i. coloque a string(cW) na sequência de saída;
-                //cout << "str insere " << dict.at(cw) << "\n";
-                str = str + dict.at(cw);
-                //cout << "string de saída atual: " << str << "\n";
-		        //ii. P <= string(pW);
-                P = P + (char)('0' + pw);
-		        //iii. C <= primeiro caracter da string(cW);
-		        //iv. adicione a string P+C ao dicionário;
-                dict.push_back(P+cstr[0]);
-                //cout << "Inserido no dicionário: " << dict1.at(dict1.size()-1) << "\n";
-        }
-        else{
-            P = P + (char)('0' + pw);
-            str = str + (P + cstr[0]);
-            //cout << "Inserido no dicionário: " << dict.at(dict.size()-1) << "\n";
-        }
-        //cout << "cw " << cw << " string " << cstr << "\n";
-    }
-    //cout << "\n Descompactei: " << str << "\n";
-    return str;
+
+vector<int> lzwCompressor(string entrada) 
+{ 
+    unordered_map<string, int> dict; 
+    for (int i = 0; i <= 255; i++) { 
+        string ascii = ""; 
+        ascii += char(i); 
+        dict[ascii] = i; 
+    } 
+  
+    string I = "", c = ""; 
+    I += entrada[0]; 
+    int code = 256; 
+    vector<int> seqcod; 
+    for (int i = 0; i < entrada.length(); i++) { 
+        if (i != entrada.length() - 1) 
+            c += entrada[i + 1]; 
+        if (dict.find(I + c) != dict.end()) { 
+            I = I + c; 
+        } 
+        else {  
+            seqcod.push_back(dict[I]); 
+            dict[I + c] = code; 
+            code++; 
+            I = c; 
+        } 
+        c = ""; 
+    } 
+    //cout << I << "\t" << dict[I] << endl; 
+    seqcod.push_back(dict[I]); 
+    return seqcod; 
+} 
+
+int main(){
+    string teste = "A_ASA_DA_CASAAAAAAA_AAAAAs";
+    cout << lzwDescompressor(lzwCompressor(teste));
+    ///cout << "Espaço economizado: " << teste.length() - lzwCompressor(teste).size() << "\n";
 }
